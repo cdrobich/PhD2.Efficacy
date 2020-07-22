60f7374c8281f8c91f86783ff7e642bbea11b839
Courtney Robichaud <cdrobich@gmail.com>
2020-07-21 21:05:20 -0400

    bigger points

--- Scripts/LiveStems_ANCOVA.R
+++ Scripts/LiveStems_ANCOVA.R
@@ -88,7 +88,7 @@ YearTwo %>% group_by(Treatment) %>% summarise(Water.avg = mean(Depth),
 Figure <- ggplot(YearTwo, aes(x = Depth, y = LiveStem, shape = Treatment, color = Treatment)) 
 
 
-Figures <- Figure + geom_point(size = 2) +
+Figures <- Figure + geom_point(size = 4) +
   stat_smooth(method = lm, level = 0.95, colour = "#999999") +
   theme_classic() +
   scale_color_manual(values = c("#d8b365","#5ab4ac")) +
